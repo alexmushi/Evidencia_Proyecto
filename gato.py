641f94fd22692050a9dcd1a06b7fe6aaedb04caf
alexmushi <A01705550@tec.mx>
2024-05-09 18:34:35 -0600

    Las X ahora son rojas y los 0 son azules

--- gato.py
+++ gato.py
@@ -13,11 +13,13 @@ def grid():
 
 # Define la función para dibujar una 'X' en la posición (x, y)
 def drawx(x, y):
+    color('red')  # Establece el color del lápiz a rojo
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
 # Define la función para dibujar un 'O' en la posición (x, y)
 def drawo(x, y):
+    color('blue')  # Establece el color del lápiz a azul
     up()  # Levanta el lápiz para no dibujar mientras se mueve
     goto(x + 67, y + 5)  # Mueve el lápiz a la posición inicial para el círculo
     down()  # Baja el lápiz para empezar a dibujar
